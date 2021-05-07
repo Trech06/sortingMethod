9fca6f2bb6d0c6836e1a02e4226f7eea5ff8d2db
”Ivan <Trech06@mail.ru>
2021-05-07 18:04:40 +0300

    MAIN modified

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
-    }
+        int[] j;
+        for (int i = 0; i<args.length; i++) {
+            if () {
+                System.out.println(args[i]);
+            }
+           }
+        }
 }
