67cad706956a8c3d1ede26819bd8817f677e6422
”Ivan <Trech06@mail.ru>
2021-05-15 17:23:57 +0300

    Somethink added

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,19 @@ public class Main {
     public static void main(String[] args) {
         int[] sorted = new int[args.length];
         ArrayList<Integer> firstArrayList = new ArrayList<>();
+        ArrayList<Integer> secondArrayList = new ArrayList<>();
+        ArrayList<Integer> thirdArrayList = new ArrayList<>();
         int tmp;
         for (int i=0; i < args.length; i++) {
             try {
                 sorted[i] = Integer.parseInt(args[i]);
                 firstArrayList.add(Integer.parseInt(args[i]));
+                secondArrayList.add(Integer.parseInt(args[i]));
             } catch (NumberFormatException e ){
                 System.out.println("Введенные строки исключены из проверки");
             }
         }
-        System.out.println("Arrays " + Arrays.toString(sorted));
+        System.out.println("Arrays    " + Arrays.toString(sorted));
         System.out.println("ArrayList " + firstArrayList.toString());
 
         for (int i=0; i < sorted.length; i++) {
@@ -26,9 +29,15 @@ public class Main {
                 }
             }
         }
-        System.out.println("ArraysSorted " + Arrays.toString(sorted));
+        thirdArrayList = firstArrayList;
+        System.out.println("ArraysSorted           " + Arrays.toString(sorted));
         Collections.sort(firstArrayList);
-        System.out.println("ArrayListSorted "+ firstArrayList.toString());
+        System.out.println("ArrayListSorted        "+ firstArrayList.toString());
+
+        Collections.sort(secondArrayList, Collections.reverseOrder());
+        System.out.println("ArrayListSortedReverse "+ secondArrayList.toString());
+
+        System.out.println("thirdArrayList " +thirdArrayList.toString());
 
         }
 }
